d2a31c5004e2f9350585ffb2c10aa7d6e86de4a1
loyaltyarm <loyaltyarm@loyaltyarm-mbp.local>
2014-07-09 22:57:53 -0700

    Initial Commit

---
+++ Cereal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cereal
+//
+//  Created by loyaltyarm on 7/9/14.
+//  Copyright (c) 2014 loyaltyarm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
